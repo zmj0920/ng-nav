98f297eb5ae5bdc51f68ff16a32ec80d9a532add
GordonLz <451813289@qq.com>
2019-12-06 18:14:20 +0800

    update

--- src/data/ui/ui.js
+++ src/data/ui/ui.js
@@ -9,132 +9,153 @@ export default {
           name: 'Dribbble',
           desc: '设计师必备站点，国内顶尖的设计师都在上面',
           link: 'https://dribbble.com/',
-          language: [
-            '',
-            '',
-            ''
-          ]
         },
         {
           icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-4-2.png',
           name: 'Behance',
           desc: '全球领先的创意设计类聚合平台',
           link: 'https://www.behance.net/',
-          language: [
-            '',
-            '',
-            ''
-          ]
         },
         {
           icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-4-3.png',
           name: 'UI8',
           desc: '聚集世界各地优秀设计师的界面源文件',
           link: 'https://ui8.net/',
-          language: [
-            '',
-            '',
-            ''
-          ]
         },
         {
           icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-4-4.png',
           name: 'Flat UI',
           desc: '扁平化UI设计灵感，采集扁平化相关的App、网页等',
           link: 'http://flatui.com/',
-          language: [
-            '',
-            '',
-            ''
-          ]
         },
         {
           icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-4-5.png',
           name: 'UI Movement',
           desc: '展示世界最有才华设计师的界面动效设计作品',
           link: 'https://uimovement.com/',
-          language: [
-            '',
-            '',
-            ''
-          ]
         },
         {
           icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-4-6.png',
           name: '365psd',
           desc: '兢兢业业每天更新着用户界面相关的PSD',
           link: 'https://365psd.com/',
-          language: [
-            '',
-            '',
-            ''
-          ]
         },
         {
           icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-4-7.png',
           name: 'CollectUI',
           desc: 'UI设计必备！100多个分类，不用发愁没灵感了',
           link: 'https://www.collectui.com/',
-          language: [
-            '',
-            '',
-            ''
-          ]
         },
         {
           icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-4-8.png',
           name: 'UI 设计',
           desc: '超赞！为您精挑细选的界面设计频道',
           link: 'https://uiiiuiii.com/inspirations/ui',
-          language: [
-            '',
-            '',
-            ''
-          ]
         },
         {
           icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-4-9.png',
           name: 'Pixeden',
           desc: '赞！免费优质界面设计源文件及有网站模板',
           link: 'https://www.pixeden.com/',
-          language: [
-            '',
-            '',
-            ''
-          ]
         },
         {
           icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-4-10.png',
           name: 'Site Inspire',
           desc: '致力于分享推荐优秀网页及交互设计案例',
           link: 'https://www.siteinspire.com/',
-          language: [
-            '',
-            '',
-            ''
-          ]
         },
         {
           icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-4-11.png',
           name: 'Designmodo',
           desc: '所有高质量UI工具包都在这里可以找到，部分免费下载',
           link: 'https://designmodo.com/',
-          language: [
-            '',
-            '',
-            ''
-          ]
         },
         {
           icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-4-12.png',
           name: '移动端设计',
           desc: '推荐！收集被程序员追着砍的App动效和视频',
           link: 'https://weibo.com/edddesign',
-          language: [
-            '',
-            '',
-            ''
-          ]
+        }
+      ]
+    },
+    {
+      subtitle: '设计工具',
+      nav: [
+        {
+          icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-9-11.png',
+          name: '墨刀标注',
+          desc: '好用的自动标注切图，快速交互设计神器',
+          link: 'https://modao.cc/?utm_source=hao_uisdc'
+        }
+      ]
+    },
+    {
+      subtitle: '素材资源',
+      nav: [
+        {
+          icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-sc-3.png',
+          name: 'LogoPond',
+          desc: '高端logo集萃，设计前必须来池子里泡一泡',
+          link: 'https://logopond.com/'
+        },
+        {
+          icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-1-1.png',
+          name: '阿里图标库',
+          desc: '国内最著名的图标搜索及管理平台，300万个图标下载',
+          link: 'https://www.iconfont.cn/'
+        },
+        {
+          icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-sc-5.png',
+          name: '矢量Logo下载',
+          desc: '超赞！知名Logo矢量资源下载',
+          link: 'https://worldvectorlogo.com/'
+        }
+      ]
+    },
+    {
+      subtitle: '配色方案',
+      nav: [
+        {
+          icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-5-3.png',
+          name: 'Coolors',
+          desc: '实用！成千上万的配色方案',
+          link: 'https://coolors.co/browser/latest/1'
+        },
+        {
+          icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-5-1.png',
+          name: 'Kuler',
+          desc: '网页设计师配色的最佳之选',
+          link: 'https://color.adobe.com/zh/'
+        },
+        {
+          icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-5-4.png',
+          name: '漂亮的渐变颜色',
+          desc: '今年流行的渐变！点击屏幕两侧按钮可选更多色彩',
+          link: 'https://uigradients.com/'
+        },
+        {
+          icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-5-6.png',
+          name: '色彩猎人',
+          desc: '每天收集并策划发布美丽的配色方案',
+          link: 'https://colorhunt.co/'
+        },
+        {
+          icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-5-7.png',
+          name: '中国色彩大辞典',
+          desc: '中国/日本传统色彩命名，点击色彩可直接吸取色值',
+          link: 'https://color.uisdc.com/'
+        },
+        {
+          icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-5-8.png',
+          name: '配色导航',
+          desc: '推荐！流行配色方案，可一键复制喜欢的颜色',
+          link: 'https://color.uisdc.com/pick.html'
+        },
+        {
+          icon: 'https://image.uisdc.com/wp-content/uploads/2018/08/sdcnav-5-11.png',
+          name: 'ColorDrop',
+          desc: '让寻找配色方案成为信手拈来的事情',
+          link: 'https://colordrop.io/'
         }
       ]
     }
